57eb440e6adcbc51755d7bfcf570c51329e91e74
Rudi <rudicondo2702@gmail.com>
2022-12-26 14:55:13 -0300

    ejemplos de route y controller

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,11 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
-
+/*
+use App\Http\Controllers\librosController;
+Route::get('/v1/libros', [librosController::class, 'obtenerLibros'])
+igual se puede hacer esto pero puede ocurrir un error si pones un name igual con namespace diferentes
+*/
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -33,15 +37,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
+
 });
-
-
-
-
-
-
-
-
 
 Route::get('/saludar',function(){
     return 'Hola mundo!';
@@ -55,10 +52,13 @@ Route::get('/v1/libros/{id}',[App\Http\Controllers\librosController::class, 'obt
 
 Route::get('/v2/libros', [App\Http\Controllers\librosController::class, 'obtenerLibrosv2'])
     ->name('apiLibros.listarv2');
+
 /*
+borrador 1 metodo post que no voy a usar
 Route::post('ejem', function(Request $request){
     $name = $request->input('name');
     $apellido = $request->input('apellido');
     $genero = $request->input('genero');
 });
+borrador 2 puedes ponerlo en group de middleware para que pida autenticacion o antes de ->name('apiLibros.x') pones ->middleware('auth:sanctum') el ";" es solo al final del parrafo del route
 */
